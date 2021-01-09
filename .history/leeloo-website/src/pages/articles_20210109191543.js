acc72d75c52662354213c73c0bb3495a580b7cb0
Yurii Drahomaretskyi <yuradrahomaretsky@gmail.com>
2021-01-09 20:01:50 +0100

    deploy try-2

---
+++ .history/leeloo-website/src/pages/articles_20210109191543.js
@@ -0,0 +1,54 @@
+// import React from 'react';
+// import styled from 'styled-components';
+// import slugify from 'slugify';
+// import { graphql } from 'gatsby';
+// import ArticlePreview from '../components/ArticlePreview/ArticlePreview';
+// import PageInfo from '../components/PageInfo/PageInfo';
+
+// const ArticlesWrapper = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(3, 1fr);
+//   grid-gap: 50px;
+// `;
+
+// const pageData = {
+//   title: 'articles',
+//   paragraph: `While artists work from real to the abstract, architects must work from the abstract to the real.`,
+// }
+
+// const ArticlesPage = ({ data }) => {
+//   const {allDatoCmsArticle: {nodes}} = data;
+//   return (
+//     <>
+//       <PageInfo title={pageData.title} paragraph={pageData.paragraph} />
+//       <ArticlesWrapper>
+//       {nodes.map(({title, featuredImage}) => (
+//         <ArticlePreview
+//           key={slugify(title, {lower: true})}
+//           title={title}
+//           image={featuredImage.fluid}
+//           slug={slugify(title, {lower: true})}
+//         />
+//       ))}
+//       </ArticlesWrapper>
+//     </>
+//   );
+// }
+
+// export const query = graphql`
+//   {
+//     allDatoCmsArticle {
+//       nodes {
+//         title
+//         author
+//         featuredImage {
+//             fluid {
+//               ...GatsbyDatoCmsFluid_tracedSVG
+//             }
+//           }
+//         }
+//     }
+//   }
+// `;
+
+// export default ArticlesPage;
