0fd88ea3e11dc9c5848edde2038e7002aaa962a7
Yurii Drahomaretskyi <yuradrahomaretsky@gmail.com>
2021-01-09 19:07:31 +0100

    prepare to deploy

--- .history/leeloo-website/src/pages/gallery_20210109190346.js
+++ .history/leeloo-website/src/pages/gallery_20210109190346.js
@@ -2,18 +2,11 @@ import React from "react"
 import { Gallery } from "gatsby-theme-gallery"
 import styled from "styled-components"
 
-const TextWrapper = styled.div`
-  text-align: center;
-  padding-bottom: 3rem;
-`
+const TextWrapper = styled.div
 
 const GalleryPage = () => (
   <>
     <Gallery />
-    <TextWrapper>
-      <h2>More photos in social media</h2>
-      <h1>⬇⬇⬇</h1>
-    </TextWrapper>
   </>
 )
 
